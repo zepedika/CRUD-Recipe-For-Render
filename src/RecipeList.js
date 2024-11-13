b2bdab18bbfe45bf63cd8c112161a5388f6c5ba9
Erika Zepeda <157064202+zepedika@users.noreply.github.com>
2024-11-13 10:42:31 -0500

    Add detailed comments to RecipeList component

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-function RecipeList({ recipes, onDeleteRecipe}) {
-  
-//List of Recipies
+/* Component renders a list of recipes in a table format. Each row displays the name, cuisine, photo, ingredients, and preparation steps for a recipe. It also includes a delete button for removing recipes from the list.
+Props:
+ * recipes: Array of recipe objects, each containing 'name', 'cuisine', 'photo', 'ingredients', and 'preparation'.
+ * onDeleteRecipe: Function to handle deletion of a recipe. It receives the index of the recipe to be deleted.
+ */
 
+function RecipeList({ recipes, onDeleteRecipe }) {
 
- return (
+  return (
     <table>
       <thead>
-        <tr >
+        <tr>
           <th>Name</th>
           <th>Cuisine</th>
           <th>Photo</th>
@@ -19,20 +22,26 @@ function RecipeList({ recipes, onDeleteRecipe}) {
       </thead>
       <tbody>
         {recipes.map((recipe, index) => (
-          <tr key={index}
-            
-            style={{ backgroundColor: index % 2 === 0 ? "beige" : "inherit" }}
+          <tr
+            key={index}
+            style={{ backgroundColor: index % 2 === 0 ? "beige" : "inherit" }} // Alternates row colors for better readability
           >
             <td>{recipe.name}</td>
             <td>{recipe.cuisine}</td>
             <td>
-              <img src={recipe.photo} alt={recipe.name}  style={{ maxWidth: "100%", maxHeight: "100%" }}/>
+              <img 
+                src={recipe.photo} 
+                alt={recipe.name} 
+                style={{ maxWidth: "100%", maxHeight: "100%" }} // Restricts image size to fit within the cell
+              />
             </td>
             <td>{recipe.ingredients}</td>
             <td>{recipe.preparation}</td>
             <td>
-              <button name="delete" onClick={() => onDeleteRecipe(index)} name="delete">
-                
+              <button 
+                name="delete" 
+                onClick={() => onDeleteRecipe(index)} // Calls onDeleteRecipe with the recipe index
+              >
                 Delete
               </button>
             </td>
